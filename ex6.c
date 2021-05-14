81b8c5e841ccc4c53dafd487b716620d4dd44eb6
Aygean Nurla <62152888+aygean219@users.noreply.github.com>
2021-05-14 13:56:04 +0300

    Add files via upload

--- ex6.c
+++ ex6.c
@@ -1,26 +1,28 @@
 //
-// lock_3.c - Using read-write lock object
+// lock_4.c - Using a semaphore
 //
-// pthread_rwlock_init()    - initialize a read-write lock object
-// pthread_rwlock_destroy() - destroy the read-write lock object
+// sem_init()
+// sem_destroy()
 //
-// pthread_rwlock_rdlock()
-// pthread_rwlock_wrlock()
-// pthread_rwlock_unlock()
+// sem_wait()
+// sem_post()
 //
 
 
 #include <stdio.h>
 #include <pthread.h>
 #include <stdlib.h>
+#include <semaphore.h>
+#include <fcntl.h>
+
 #define MAX_READ  1000   // maximum number of readers
 #define MAX_WRITE 100    // maximum number of writers
 
 int seats = 100;        // initial number of seats
 float price = 75.0f;    // initial price per seat
 
-//declare a read-write lock object
-pthread_rwlock_t rwl;
+// declare a semaphore
+sem_t sem;
 
 //
 // Readers start routine
@@ -29,12 +31,12 @@ void* check(void* a)
 {
 	int k = *(int*)a;
 
-	pthread_rwlock_rdlock(&rwl);      // lock for read
+	sem_wait(&sem);		// lock
 
 	printf("Reader %2d: Locuri %d, pret %f\n",
 		k, seats, price * (2 - seats/100.0f));
 
-	pthread_rwlock_unlock(&rwl);      // unlock
+	sem_post(&sem);		// unlock
 
 	return NULL;
 }
@@ -46,13 +48,13 @@ void* buy(void* a)
 {
 	int k = *(int*)a;
 
-	pthread_rwlock_wrlock(&rwl);      // lock for write
+	sem_wait(&sem);		// lock
 
 	printf("Writer %2d: Locul meu este %d, pret %f\n",
 		k, seats, price * (2 - seats/100.0f));
 	seats--;
 
-	pthread_rwlock_unlock(&rwl);      // unlock
+	sem_post(&sem);         // unlock
 
 	return NULL;
 }
@@ -61,21 +63,21 @@ void* buy(void* a)
 
 int main(int argc, char* argv[])
 {
-	// create the read-write lock object
-	pthread_rwlock_init(&rwl, NULL);
+	// create the semaphore
+	sem_init(&sem, 0, 1);
 
 	int i;
 	pthread_t tr[MAX_READ];
 	for (i = 0; i < MAX_READ; i++)
 	{
-		 int *tid = (int*)malloc(1 * sizeof(int));
-		 *tid = i;
-		pthread_create(&tr[i], NULL, check, tid);	// create readers
+		int *tid = (int*)malloc(1 * sizeof(int));
+		*tid = i;
+		pthread_create(&tr[i], NULL, check,tid);	// create readers
 	}
 
 	pthread_t tw[MAX_WRITE];
 	for (i = 0; i < MAX_WRITE; i++)
-	{	
+	{
 		int *tid = (int*)malloc(1 * sizeof(int));
 		*tid = i;
 		pthread_create(&tw[i], NULL, buy, tid);	// create writers
@@ -91,8 +93,8 @@ int main(int argc, char* argv[])
 		pthread_join(tw[i], NULL);      // wait for writers to finish
 	}
 
-	// destroy the read-write lock object
-	pthread_rwlock_destroy(&rwl);
+	// destroy the semaphore
+	sem_destroy(&sem);
 
 	return 0;
 }
